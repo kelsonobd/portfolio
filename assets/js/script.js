8c273183f2c829faf036fff868af9d07cbff519c
Kelsonobd <kelsonobd@gmail.com>
2024-10-07 11:56:15 -0300

    ajuste geral

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,34 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const menuOpener = document.querySelector('.menu-opener');
-    const nav = document.querySelector('nav');
+document.addEventListener('DOMContentLoaded', function () {
+    const projectItems = document.querySelectorAll('.project-item img');
+    const overlay = document.querySelector('.project-overlay');
+    const overlayImage = document.getElementById('enlarged-image');
+    const descriptionDiv = overlay.querySelector('.description');
+    const closeBtn = overlay.querySelector('.close-btn');
 
-    menuOpener.addEventListener('click', function() {
-        if (nav.style.display === 'block') {
-            nav.style.display = 'none';
-        } else {
-            nav.style.display = 'block';
+    projectItems.forEach(item => {
+        item.addEventListener('click', function () {
+            const projectDescription = this.parentElement.querySelector('.project-description');
+            const linkUrl = this.dataset.link; // Obtém o link do data-link
+            
+            overlay.style.display = 'flex';
+            overlayImage.src = item.src;
+
+            // Exibe a descrição que está dentro da tag <p>
+            descriptionDiv.innerHTML = projectDescription.innerHTML;
+
+            // Atualiza o link dentro da descrição
+            const linkElement = descriptionDiv.querySelector('a');
+            linkElement.href = linkUrl; // Define o href do link de acordo com o data-link
+        });
+    });
+
+    closeBtn.addEventListener('click', function () {
+        overlay.style.display = 'none';
+    });
+
+    overlay.addEventListener('click', function (e) {
+        if (e.target === overlay) {
+            overlay.style.display = 'none';
         }
     });
 });
