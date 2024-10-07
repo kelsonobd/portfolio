d1b5a7818c2dabd1c742037caeca31c7c0eb090c
Kelsonobd <kelsonobd@gmail.com>
2024-10-07 13:19:07 -0300

    adicionado efeito no titulo principal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,45 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const projectItems = document.querySelectorAll('.project-item img');
-    const overlay = document.querySelector('.project-overlay');
-    const overlayImage = document.getElementById('enlarged-image');
-    const descriptionDiv = overlay.querySelector('.description');
-    const closeBtn = overlay.querySelector('.close-btn');
+document.addEventListener("DOMContentLoaded", function () {
+    const projectItems = document.querySelectorAll(".project-item img");
+    const overlay = document.querySelector(".project-overlay");
+    const overlayImage = document.getElementById("enlarged-image");
+    const descriptionDiv = overlay.querySelector(".description");
+    const closeBtn = overlay.querySelector(".close-btn");
 
-    projectItems.forEach(item => {
-        item.addEventListener('click', function () {
-            const projectDescription = this.parentElement.querySelector('.project-description');
+    projectItems.forEach((item) => {
+        item.addEventListener("click", function () {
+            const projectDescription = this.parentElement.querySelector(
+                ".project-description"
+            );
             const linkUrl = this.dataset.link; // Obtém o link do data-link
-            
-            overlay.style.display = 'flex';
+
+            overlay.style.display = "flex";
             overlayImage.src = item.src;
 
             // Exibe a descrição que está dentro da tag <p>
             descriptionDiv.innerHTML = projectDescription.innerHTML;
 
             // Atualiza o link dentro da descrição
-            const linkElement = descriptionDiv.querySelector('a');
+            const linkElement = descriptionDiv.querySelector("a");
             linkElement.href = linkUrl; // Define o href do link de acordo com o data-link
         });
     });
 
-    closeBtn.addEventListener('click', function () {
-        overlay.style.display = 'none';
+    closeBtn.addEventListener("click", function () {
+        overlay.style.display = "none";
     });
 
-    overlay.addEventListener('click', function (e) {
+    overlay.addEventListener("click", function (e) {
         if (e.target === overlay) {
-            overlay.style.display = 'none';
+            overlay.style.display = "none";
         }
+    });
+
+    // Inicializa o efeito Typed.js
+    var typed = new Typed(".text", {
+        strings: ["Desenvolvedor Front-end"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
     });
 });
